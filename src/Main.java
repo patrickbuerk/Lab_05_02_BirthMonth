a558281434ff3df849dc2c50f7f74a9bbbc3cae3
patrickbuerk <98759563+patrickbuerk@users.noreply.github.com>
2022-04-03 16:06:22 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main
+
+{
+
+    public static void main(String[] args)
+    {
+        int birthMonth = 1;
+        int birthMonthWrong = 13;
+        if (birthMonthWrong>=1){
+
+            if (birthMonthWrong<=12){
+                System.out.println("your birth month is "+birthMonthWrong);
+
+            }
+            else {
+                System.out.println("you entered the incorrect birth month value the value you input was " +birthMonthWrong);
+            }
+        }
+
+        if (birthMonth >=1){
+
+            if (birthMonth <=12){
+                System.out.println("your birth month is "+birthMonth);
+                
+            }
+            else{
+                System.out.println("you entered an incorrect birth month value the value you entered is "+birthMonth);
+            }
+        }
+
+
+
+    }
+
+
+
+
+    }
+
